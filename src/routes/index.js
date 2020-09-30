f79bc4850d3bcb2637c097405a2226484d7e5397
Kazdan1994 <arthurjacquemin2@gmail.com>
2020-09-30 14:02:15 +0200

    feat(server): add mongodb

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+const Post = require('../db/models/postModel');
+
+module.exports = function () {
+    const app = Router();
+
+    app.get('/', async function (req, res) {
+        const posts = await Post.find();
+
+        res.render('index', {
+            posts
+        })
+    })
+    app.get('/post', function (req, res) {
+        res.render('show')
+    })
+
+    return app;
+}
