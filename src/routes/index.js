03c85b29f2efb9fb4b08fb6964372817e78ff0c5
Kazdan1994 <arthurjacquemin2@gmail.com>
2020-09-30 16:00:42 +0200

    feat(server): add edit view

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,18 +11,32 @@ module.exports = function () {
             posts
         })
     })
+    app.get('/blog/new', async function (req, res) {
+        res.render('new')
+    })
+    app.post('/blog/new', async function (req, res) {
+        await Post.create(req.body);
+
+        res.redirect('/');
+    })
+
+    app.get('/blog/edit/:id', async function (req, res) {
+        const post = await Post.findById(req.params.id);
+
+        res.render('edit', {
+            post
+        })
+    })
+    app.put('/blog/edit/:id', async function (req, res) {
+        console.log(req.body);
+    })
+
     app.get('/post/:id', async function (req, res) {
         const post = await Post.findById(req.params.id);
 
         res.render('show', {
             post
         })
-    })
-    app.get('/blog/new', async function (req, res) {
-        res.render('new')
-    })
-    app.post('/blog/new', async function (req, res) {
-        console.log(req.body);
     })
 
     return app;
