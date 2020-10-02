df13ec525cb902bdfe834856344f34840d2cf824
Kazdan1994 <arthurjacquemin2@gmail.com>
2020-10-02 15:04:16 +0200

    feat(server): add validation

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,4 +3,6 @@ const AuthController = require('../controller/authController');
 module.exports = function (app) {
     app.get('/register', AuthController.register)
     app.get('/login', AuthController.login)
+
+    app.post('/register', AuthController.signup);
 }
