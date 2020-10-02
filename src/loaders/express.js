fe14b11b34555c181f086c14ec4cedf0367752fc
Kazdan1994 <arthurjacquemin2@gmail.com>
2020-10-02 15:13:58 +0200

    feat(server): sessions

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -2,17 +2,8 @@ const express = require('express');
 const path = require('path');
 const { flash } = require('express-flash-message');
 const routes = require('../routes');
-const session = require("express-session");
-const cookieParser = require('cookie-parser');
 
 module.exports = function (app) {
-    app.use(cookieParser());
-    app.use(session({
-        secret: "1234567890QWERTY",
-        resave: true,
-        store: new session.MemoryStore(),
-        saveUninitialized: true
-    }));
 
     app.use('/', express.static(path.resolve(__dirname, '../../public')));
     app.set('views', path.resolve(__dirname, '../views'));
--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,8 +1,10 @@
 const expressLoader = require('./express');
 const ejsLoader = require('./ejs');
 const mongooseLoader = require('./mongoose');
+const sessionLoader = require('./session');
 
 module.exports = async function (app) {
+    await sessionLoader(app);
     await expressLoader(app);
     await ejsLoader(app);
     await mongooseLoader();
